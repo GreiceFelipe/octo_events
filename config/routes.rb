635b5fa2d79f4802e0cbc97d9f6165db5e6a29b3
Nywton Barros <nywton.barros@gmail.com>
2018-06-27 07:39:36 -0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :events, only: [:create, :update, :show]
   resources :issues, only: [:index, :show] do
-    resources :events, except: [:destroy]
+    resources :events, only: [:index]
   end
 end
