94de530270eb83555fb7ce274f39866dbd4d57f1
Nywton Barros <nywton.barros@gmail.com>
2018-06-27 17:38:19 -0300

    Added API doc, added Issues docs

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Octo Events",
+  "version": "0.1.0",
+  "description": "Api methods",
+  "title": "COcto Events API",
+  "url": "https://octoevents.herokuapp.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-06-27T20:37:40.705Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
