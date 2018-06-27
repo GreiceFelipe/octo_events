a1b2f395bd36da2585cd078b5344555effe8a2e8
Nywton Barros <nywton.barros@gmail.com>
2018-06-27 20:06:54 -0300

    Fix spec

--- spec/services/events/create_spec.rb
+++ spec/services/events/create_spec.rb
@@ -31,8 +31,17 @@ RSpec.describe Events::Create do
     described_class.new(valid_params)
   end
 
+  subject(:create_event_service_with_invalid_params) do
+    described_class.new(invalid_params)
+  end
+
   specify do
     create_event_service.call
-    expect(create_event_service.success?).to be(true)
+    expect(create_event_service.success?).to be_truthy
+  end
+
+  specify do
+    create_event_service_with_invalid_params.call
+    expect(create_event_service_with_invalid_params.success?).to be_falsey
   end
 end
