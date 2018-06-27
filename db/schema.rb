18ddafdf4ac763795e2f240cca6bc9db94b60377
Nywton Barros <nywton.barros@gmail.com>
2018-06-26 21:38:10 -0300

    Events scaffold and event model specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_000438) do
+ActiveRecord::Schema.define(version: 2018_06_27_002413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "action"
+    t.bigint "issue_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["issue_id"], name: "index_events_on_issue_id"
+  end
 
   create_table "issues", force: :cascade do |t|
     t.integer "number"
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 2018_06_27_000438) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "issues"
 end
---
+++ spec/routing/events_routing_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe EventsController, type: :routing do
+  describe "routing" do
+
+  end
+end
